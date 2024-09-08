6438a3f0cd7a765a0719d97e3af2f365d713d5cc
Fab0705 <1cocharcaschorrillos@gmail.com>
2024-09-08 17:41:04 -0500

    SemiFull Design

--- js/script.js
+++ js/script.js
@@ -1,33 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const dropdownBtnContact = document.querySelector('.dropdown-contact');
-    const dropdownContentContact = document.querySelector('.content-contact');
-
-    dropdownBtnContact.addEventListener('click', function() {
-        // Alterna la visibilidad del contenido de contacto
-        dropdownContentContact.style.display = dropdownContentContact.style.display === 'block' ? 'none' : 'block';
-    });
-
-    // Cierra el desplegable si se hace clic en cualquier otro lugar de la página
-    window.addEventListener('click', function(e) {
-        if (!dropdownBtnContact.contains(e.target) && !dropdownContentContact.contains(e.target)) {
-            dropdownContentContact.style.display = 'none';
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     const dropdownBtns = document.querySelectorAll('.dropdown-btn');
     const contentUsil = document.querySelector('.content-usil');
     const contentIdat = document.querySelector('.content-idat');
     const dropdownContent = document.querySelector('.dropdown-content');
     const closeBtns = document.querySelectorAll('.close-btn');
+    const dropdownBtnContact = document.querySelector('.dropdown-contact');
+    const dropdownContentContact = document.querySelector('.content-contact');
 
     dropdownBtns.forEach((btn, index) => {
         btn.addEventListener('click', function() {
-            // Muestra el contenido debajo de los botones
             dropdownContent.classList.add('active');
             
-            // Muestra el contenido correspondiente según el botón que se ha clicado
             if (index == 0) {
                 contentUsil.classList.add('active');
                 contentIdat.classList.remove('active');
@@ -45,5 +28,26 @@ document.addEventListener('DOMContentLoaded', function() {
             contentUsil.classList.remove('active');
             contentIdat.classList.remove('active');
         });
+    });
+
+    dropdownBtnContact.addEventListener('click', function() {
+        dropdownContentContact.style.display = dropdownContentContact.style.display === 'block' ? 'none' : 'block';
+    });
+
+    // Cierra el desplegable si se hace clic en cualquier otro lugar de la página
+    /* window.addEventListener('click', function(e) {
+        if (!dropdownBtnContact.contains(e.target) && !dropdownContentContact.contains(e.target)) {
+            dropdownContentContact.style.display = 'none';
+        }
+    }); */
+
+    //Circle level language
+
+    const circles = document.querySelectorAll('.circle');
+
+    circles.forEach(circle => {
+        const percent = circle.getAttribute('data-percent');
+        
+        circle.style.background = `conic-gradient(#fdc12d ${percent * 3.6}deg, #3b3b47 0deg)`;
     });
 });
