4cc689f926e42abee41a2c55336a7702913df326
Fab0705 <1cocharcaschorrillos@gmail.com>
2024-09-13 18:40:33 -0500

    Multi-language

--- js/script.js
+++ js/script.js
@@ -80,3 +80,30 @@ window.addEventListener('click', (event) => {
         event.target.classList.add('fade');
     }
 });
+
+function toggleAside() {
+    const aside = document.getElementById('short-information');
+    aside.classList.toggle('open');
+    aside.classList.toggle('closed');
+}
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async language=>{
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange)
+    {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+};
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
