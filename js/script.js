8639d716059a26d596722788aae7b2e5f16efa4a
Fab0705 <1cocharcaschorrillos@gmail.com>
2024-09-14 21:08:53 -0500

    Education-Modify

--- js/script.js
+++ js/script.js
@@ -1,39 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const dropdownBtns = document.querySelectorAll('.dropdown-btn');
-    const contentUsil = document.querySelector('.content-usil');
-    const contentIdat = document.querySelector('.content-idat');
-    const dropdownContent = document.querySelector('.dropdown-content');
-    const closeBtns = document.querySelectorAll('.close-btn');
-    const dropdownBtnContact = document.querySelector('.dropdown-contact');
-    const dropdownContentContact = document.querySelector('.content-contact');
-    
-    dropdownBtns.forEach((btn, index) => {
-        btn.addEventListener('click', function() {
-            dropdownContent.classList.add('active');
-            
-            if (index == 0) {
-                contentUsil.classList.add('active');
-                contentIdat.classList.remove('active');
-            } else {
-                contentIdat.classList.add('active');
-                contentUsil.classList.remove('active');
-            }
-        });
-    });
-
-    // Cerrar al hacer clic en el botón "Close"
-    closeBtns.forEach((btn) => {
-        btn.addEventListener('click', function() {
-            dropdownContent.classList.remove('active');
-            contentUsil.classList.remove('active');
-            contentIdat.classList.remove('active');
-        });
-    });
-
-    dropdownBtnContact.addEventListener('click', function() {
-        dropdownContentContact.style.display = dropdownContentContact.style.display === 'block' ? 'none' : 'block';
-    });
-
     //Circle level language
 
     const circles = document.querySelectorAll('.circle');
